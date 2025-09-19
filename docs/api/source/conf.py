b4b913e31ed363536ea253f02089f80c9ea31e94
Fernando Magalhaes <fmagalhaes@carelink.com.br>
2025-09-19 04:19:56 -0300

    feat(uml): import Modelio project for User Management domain

--- docs/api/source/conf.py
+++ docs/api/source/conf.py
@@ -21,7 +21,7 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 
-html_theme = 'sphinx_rtd_theme'  # Mude de alabaster para este tema
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 
